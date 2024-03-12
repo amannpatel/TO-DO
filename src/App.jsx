0bb912f5aa17f710afb1cea786d581f51f4f6bc9
Aman Patel <aman17626@gmail.com>
2024-03-12 11:38:46 +0530

    Add ErrorPage and routing logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import ErrorPage from "./components/ErrorPage";
 function App() {
   return (
     <>
       <h1 className="text-red-400">Todo App</h1>
+      <ErrorPage />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Dashboard from "./components/Dashboard.jsx";
+import LoginPage from "./components/LoginPage.jsx";
+import ErrorPage from "./components/ErrorPage.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <LoginPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "todo",
+    element: <Dashboard />,
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={appRouter} />);
